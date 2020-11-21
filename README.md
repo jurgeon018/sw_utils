85fd10c3036d54a8b0050b204c7e71a17b1b3a1e
mendela andrew <jurgeon018@gmail.com>
2020-11-21 14:34:56 +0200

    automatic updating commit

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-requirements
 
 
-modeltranslation
+## Set language 
+
+templates/projects/header.html
+{% get_current_language as LANGUAGE_CODE %}
+{% get_available_languages as LANGUAGES %}
+{% get_language_info_list for LANGUAGES as languages %}
+{% for language in languages %}
+<a href="{% url 'set_lang' language.code %}">
+<div class="nav-lang {% if language.code == LANGUAGE_CODE %}active{% endif %}">
+  {% if  language.code == 'uk' %}
+    Ukr
+  {% elif language.code == 'ru' %}
+    Rus
+  {% elif language.code == 'en' %}
+    Eng
+  {% endif %}
+</div>
+</a>
+{% endfor %}
+
+
--- urls.py
+++ urls.py
@@ -62,32 +62,32 @@ def generate_sitemaps():
 
   return sitemaps
 
-def get_box_apps():
-  box_apps = [
-    # 'sw_utils',
-    'sw_modelclone',
-    'sw_global_config',
-    'sw_solo',
-    'sw_watermarker',
-    'sw_model_search',
-    'sw_currency',
-    'sw_content',
-    'sw_contact_form',
-    'sw_auth',
-    'sw_delivery',
-    'sw_novaposhta',
-    'sw_delivery_auto',
-    'sw_sat',
-    'sw_catalog',
-    'sw_order',
-    'sw_cart',
-    'sw_customer',
-    'sw_liqpay',
-    'sw_wayforpay',
-    'sw_interkassa',
-    'sw_blog',
-  ]
-  return box_apps
+# def get_box_apps():
+#   box_apps = [
+#     # 'sw_utils',
+#     'sw_modelclone',
+#     'sw_global_config',
+#     'sw_solo',
+#     'sw_watermarker',
+#     'sw_model_search',
+#     'sw_currency',
+#     'sw_content',
+#     'sw_contact_form',
+#     'sw_auth',
+#     'sw_delivery',
+#     'sw_novaposhta',
+#     'sw_delivery_auto',
+#     'sw_sat',
+#     'sw_catalog',
+#     'sw_order',
+#     'sw_cart',
+#     'sw_customer',
+#     'sw_liqpay',
+#     'sw_wayforpay',
+#     'sw_interkassa',
+#     'sw_blog',
+#   ]
+#   return box_apps
 
 def generate_urlpatterns():
   PROJECT_CORE              = [path('', include(url)) for url in core_settings.PROJECT_CORE_URLS]
@@ -106,20 +106,12 @@ def generate_urlpatterns():
   box_multilingual = []
 
   for app in box_apps:
-    # try:
-    #   box.append(path('', include(f'{app}.urls')))
-    # except ImportError as i_e:
-    #   pass 
     urls_path = f'{app}.urls'
     if find_spec(urls_path) is not None:
       url = path('', include(urls_path))
       box.append(url)
         
   for app in box_apps:
-    # try:
-    #   box_multilingual.append(path('', include(f'{app}.multilingual_urls')))
-    # except ImportError as i_e:
-    #   pass 
     urls_path = f'{app}.multilingual_urls'
     if find_spec(urls_path) is not None:
       url = path('', include(urls_path))
@@ -145,36 +137,19 @@ def generate_urlpatterns():
     static_urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
   urlpatterns = [
+    *PROJECT_CORE,
+    *multilingual,
+    *box,
     *static_urlpatterns,
     path('sitemap.xml/',     sitemap, {'sitemaps':generate_sitemaps()}, name='sitemap'),
     path('robots.txt/',      robots,           name='robots'),
-    # path('sitemap.xml/',     cache_page(60)(sitemap), {'sitemaps': sitemaps}, name='cached-sitemap'),
-    # path('robots.txt/',      include('robots.urls')),
     path('i18n/',            include('django.conf.urls.i18n')),
-
     path('set_lang/<new_lang>/', set_lang,     name="set_lang"),
-    # path('setlang/',             set_language, name="set_lang"),
     path('jsi18n/',          js_cat.as_view(), name='javascript-catalog'),
-    # path('admin_tools/',     include('admin_tools.urls')),
-    # path('grappelli/',       include('grappelli.urls')),
     path('tinymce/',         include('tinymce.urls')),
-
-    # path('froala_editor/',   include('froala_editor.urls')),
-    # path('ckeditor/',        include('ckeditor_uploader.urls')),
-    # path('summernote/',      include('django_summernote.urls')),
-    # path('markdown/',        include('django_markdown.urls')),
-    # path('markdownx/',       include('markdownx.urls')),
-
-    # path('filer/',           include('filer.urls')),
     path('api-auth/',        include('rest_framework.urls', namespace='rest_framework')),
-    # path('auth/',            include('djoser.urls')),
-    # path('auth/',            include('djoser.urls.authtoken')),
-    # path('auth/',            include('djoser.urls.jwt')),
     path('_nested_admin/',   include('nested_admin.urls')),
     path('filebrowser/',     site.urls),
-    *box,
-    *PROJECT_CORE,
-    *multilingual,
   ]
 
   if core_settings.DJANGO_DEBUG_TOOLBAR_ON and settings.DEBUG:

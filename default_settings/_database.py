aec5f82db9d94488899d7023d5d9a95723b3627e
andrew mendela <jurgeon018@gmail.com>
2021-02-10 21:54:49 +0000

    fixed cryptography

--- default_settings/_database.py
+++ default_settings/_database.py
@@ -1,23 +1,21 @@
 from ._django import BASE_DIR, os, config 
 
-SQLITE = {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-}
-POSTGRES = {
-    'ENGINE':    'django.db.backends.postgresql_psycopg2',
-    'NAME':      config('POSTGRES_DB_NAME'),
-    'USER' :     config('POSTGRES_DB_USERNAME'),
-    'PASSWORD' : config('POSTGRES_DB_PASSWORD'),
-    'HOST' :     config('POSTGRES_DB_HOST') or '127.0.0.1',
-    'PORT' :     config('POSTGRES_DB_PORT') or '5432',
-}
 if config('DB') == 'postgres':
-    default = POSTGRES
+    default = {
+        'ENGINE':    'django.db.backends.postgresql_psycopg2',
+        'NAME':      config('POSTGRES_DB_NAME'),
+        'USER' :     config('POSTGRES_DB_USERNAME'),
+        'PASSWORD' : config('POSTGRES_DB_PASSWORD'),
+        'HOST' :     config('POSTGRES_DB_HOST') or '127.0.0.1',
+        'PORT' :     config('POSTGRES_DB_PORT') or '5432',
+    }
 elif config('DB') == 'mysql':
-    default = MYSQL
+    default = {}
 else:
-    default = SQLITE
+    default = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 DATABASES = {
     'default': default,
 }

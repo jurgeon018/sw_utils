f80bc44aa56ce9be424e3136f687d5faf6cc066d
mendela andrew <jurgeon018@gmail.com>
2021-02-18 20:32:14 +0200

    fixed decouple

--- default_settings/_email.py
+++ default_settings/_email.py
@@ -4,7 +4,7 @@ IGNORABLE_404_URLS = [
     re.compile(r'\.(php|cgi)$'),
     re.compile(r'^/phpmyadmin/'),
 ]
-MAIL_TYPE = config('MAIL_TYPE')
+MAIL_TYPE = config('MAIL_TYPE', None)
 if MAIL_TYPE == 'from_settings':
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 else:
@@ -14,7 +14,7 @@ EMAIL_USE_SSL          = False
 EMAIL_PORT             = 587
 EMAIL_HOST             = "mail.starwayua.com"
 EMAIL_HOST_USER        = "dev@starwayua.com"
-EMAIL_HOST_PASSWORD    = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD    = config('EMAIL_HOST_PASSWORD', None)
 DEFAULT_FROM_EMAIL     = EMAIL_HOST_USER
 
 SERVER_EMAIL = 'dev@starwayua.com'
